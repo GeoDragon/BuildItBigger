1fada7e8a73ab122b4ee20542c5330329dd3e6d3
GeoDragon <geolangsat@gmail.com>
2016-10-03 17:35:41 +0530

    Initial commit

---
+++ jokeandroidlib/src/main/java/com/udacityprojects/geonarz/jokeandroidlib/JokesActivity.java
@@ -0,0 +1,22 @@
+package com.udacityprojects.geonarz.jokeandroidlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.mkash32.jokeandroidlib.R;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+        TextView tv_joke = (TextView) findViewById(R.id.tv_joke);
+        String joke = getIntent().getStringExtra("joke");
+        if(joke == null) {
+            tv_joke.setText("Joke unavailable");
+        }
+        tv_joke.setText(joke);
+    }
+}
